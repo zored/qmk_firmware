916786eb51e8dc98daff21010a2082ddb45b1a51
Robert <robert@robert.pro>
2019-03-07 00:34:04 +0300

    Add tap dancing

--- keyboards/ergodox_ez/keymaps/zored/config.h
+++ keyboards/ergodox_ez/keymaps/zored/config.h
@@ -1,20 +1,18 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
+// https://docs.qmk.fm/#/config_options
 #undef TAPPING_TERM
-#define TAPPING_TERM 180
+#define TAPPING_TERM 120
 
 #undef MOUSEKEY_MAX_SPEED
 #define MOUSEKEY_MAX_SPEED 10
 
 #undef MOUSEKEY_WHEEL_TIME_TO_MAX
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 80
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 150
 
 #undef MOUSEKEY_WHEEL_MAX_SPEED
-#define MOUSEKEY_WHEEL_MAX_SPEED 5
+#define MOUSEKEY_WHEEL_MAX_SPEED 4
 
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_leader_key.md
 // #define LEADER_TIMEOUT 300
 
 #define UNICODE_SELECTED_MODES UC_OSX, UC_WINC
+
--- keyboards/ergodox_ez/keymaps/zored/rules.mk
+++ keyboards/ergodox_ez/keymaps/zored/rules.mk
@@ -1,8 +1,8 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+# https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+BOOTMAGIC_ENABLE = full
 COMMAND_ENABLE = no
 RGBLIGHT_ENABLE = no
+STENO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 UNICODE_ENABLE = no
 UNICODEMAP_ENABLE = yes
-STENO_ENABLE = yes
-BOOTMAGIC_ENABLE = full

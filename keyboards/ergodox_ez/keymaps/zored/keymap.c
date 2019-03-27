e8119dbabe520205833a8180b073dbca4152dd68
zored <zored.box@gmail.com>
2019-03-27 14:10:31 +0300

    Add tap dance for number-F

--- keyboards/ergodox_ez/keymaps/zored/keymap.c
+++ keyboards/ergodox_ez/keymaps/zored/keymap.c
@@ -212,6 +212,16 @@ enum dance_keys {
   DNC_F8,
   DNC_F9,
   DNC_F10,
+  DNC_KF1,
+  DNC_KF2,
+  DNC_KF3,
+  DNC_KF4,
+  DNC_KF5,
+  DNC_KF6,
+  DNC_KF7,
+  DNC_KF8,
+  DNC_KF9,
+  DNC_KF10,
   // Last:
   DNC_AMOUNT
 };
@@ -430,7 +440,7 @@ void on_dance_reset(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 #define DANCE_MODIFIER() ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, on_dance, on_dance_reset, TAPPING_TERM_TAP_DANCE)
-#define DANCE_F(k1,k11) ACTION_TAP_DANCE_DOUBLE_TIME(k1, k11, TAPPING_TERM_TAP_DANCE)
+#define DANCE_TWO(k1,k11) ACTION_TAP_DANCE_DOUBLE_TIME(k1, k11, TAPPING_TERM_TAP_DANCE)
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Left (ctrl - alt - gui)
@@ -444,16 +454,28 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [DNC_COMM] = DANCE_MODIFIER(),
 
     // F-dance:
-    [DNC_F1] = DANCE_F(KC_F1, KC_F11),
-    [DNC_F2] = DANCE_F(KC_F2, KC_F12),
-    [DNC_F3] = DANCE_F(KC_F3, KC_F13),
-    [DNC_F4] = DANCE_F(KC_F4, KC_F14),
-    [DNC_F5] = DANCE_F(KC_F5, KC_F15),
-    [DNC_F6] = DANCE_F(KC_F6, KC_F16),
-    [DNC_F7] = DANCE_F(KC_F7, KC_F17),
-    [DNC_F8] = DANCE_F(KC_F8, KC_F18),
-    [DNC_F9] = DANCE_F(KC_F9, KC_F19),
-    [DNC_F10]= DANCE_F(KC_F10,KC_F20),
+    [DNC_F1] = DANCE_TWO(KC_F1, KC_F11),
+    [DNC_F2] = DANCE_TWO(KC_F2, KC_F12),
+    [DNC_F3] = DANCE_TWO(KC_F3, KC_F13),
+    [DNC_F4] = DANCE_TWO(KC_F4, KC_F14),
+    [DNC_F5] = DANCE_TWO(KC_F5, KC_F15),
+    [DNC_F6] = DANCE_TWO(KC_F6, KC_F16),
+    [DNC_F7] = DANCE_TWO(KC_F7, KC_F17),
+    [DNC_F8] = DANCE_TWO(KC_F8, KC_F18),
+    [DNC_F9] = DANCE_TWO(KC_F9, KC_F19),
+    [DNC_F10]= DANCE_TWO(KC_F10,KC_F20),
+
+    // Number F-dance:
+    [DNC_KF1] = DANCE_TWO(KC_1, KC_F11),
+    [DNC_KF2] = DANCE_TWO(KC_2, KC_F12),
+    [DNC_KF3] = DANCE_TWO(KC_3, KC_F13),
+    [DNC_KF4] = DANCE_TWO(KC_4, KC_F14),
+    [DNC_KF5] = DANCE_TWO(KC_5, KC_F15),
+    [DNC_KF6] = DANCE_TWO(KC_6, KC_F16),
+    [DNC_KF7] = DANCE_TWO(KC_7, KC_F17),
+    [DNC_KF8] = DANCE_TWO(KC_8, KC_F18),
+    [DNC_KF9] = DANCE_TWO(KC_9, KC_F19),
+    [DNC_KF10]= DANCE_TWO(KC_0, KC_F10),
 };
 
 enum custom_keycodes {
@@ -470,7 +492,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_DEF] = LAYOUT_ergodox(
-    KC_ESC,           KC_1,          KC_2,          KC_3,           KC_4,           KC_5,           _______,
+    KC_ESC,           TD(DNC_KF1),   TD(DNC_KF2),   TD(DNC_KF3),    TD(DNC_KF4),    TD(DNC_KF5),           _______,
     KC_TAB,           KC_Q,          KC_W,          KC_E,           KC_R,           KC_T,           _______,
     LT(L_SYM,KC_CLCK),KC_A,          KC_S,          KC_D,           KC_F,           KC_G,
     KC_LSPO,          TD(DNC_Z),     TD(DNC_X),     TD(DNC_C),      KC_V,           KC_B,           KC_MEH,
@@ -478,7 +500,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                     KC_ESC,         _______,
                                                                                                     KC_HOME,
                                                                     KC_SPC,         KC_BSPC,        KC_END,
-        OSL(L_SYM),     KC_6,           KC_7,           KC_8,           KC_9,           KC_0,             KC_RBRC,
+        OSL(L_SYM),     TD(DNC_KF6),    TD(DNC_KF7),    TD(DNC_KF8),    TD(DNC_KF9),    TD(DNC_KF10),     KC_RBRC,
         _______,        KC_Y,           KC_U,           KC_I,           KC_O,           LT(L_NAV,KC_P),   KC_LBRC,
                         KC_H,           KC_J,           KC_K,           KC_L,           LT(L_NAV,KC_SCLN),LT(L_EMO, KC_QUOT),
         KC_HYPR,        KC_N,           KC_M,           TD(DNC_COMM),   TD(DNC_DOT),    TD(DNC_SLSH),     KC_RSPC,
@@ -617,12 +639,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint32_t layer_state_set_user(uint32_t state) {
   dim_leds();
 
+  ergodox_led_all_set(LED_BRIGHTNESS_LO);
   switch (biton32(state)) {
-    case L_EMO:
-      ergodox_right_led_2_on();
+    case L_NAV:
+      ergodox_right_led_on(1);
+      break;
+    case L_SYM:
+      ergodox_right_led_on(2);
       break;
     case L_PLO:
-      ergodox_right_led_3_on();
+      ergodox_right_led_on(3);
+      break;
+    case L_EMO:
+      ergodox_right_led_on(1);
+      ergodox_right_led_on(3);
       break;
   }
 

83778488eb3ab1c100a1d08b90a329be6e72e236
Robert Akhmerov <zored.box@gmail.com>
2021-03-12 23:19:01 +0300

    [zkf_stable_planck] Add interruption check

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -193,8 +193,9 @@ bool process_record_quantum(keyrecord_t *record) {
 #endif
 
 #ifdef TAP_DANCE_ENABLE
-    preprocess_tap_dance(keycode, record);
-    keycode = get_record_keycode(record);
+    if (preprocess_tap_dance(keycode, record)) {
+        keycode = get_record_keycode(record);
+    }
 #endif
 
     if (!(
